0450a31829fac2ee869e7bf32ba886ab8881df8b
Kersten Kriegbaum <kersten@badmomber.com>
2021-09-01 10:08:24 +0200

    update node logger app

--- node_log_generator/dock.sh
+++
@@ -1,4 +0,0 @@
-docker build -t nodejs-json-logger .
-docker tag nodejs-json-logger badmomber/nodejs-json-logger:latest
-docker push badmomber/nodejs-json-logger:latest
-docker tag nodejs-json-logger badmomber/nodejs-json-logger:latest
